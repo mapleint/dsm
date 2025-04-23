abcb306678ef26def93677906a71c5ce79d9d94a
Danny Villanueva <mapleint@mit.edu>
2025-04-23 19:50:25 -0400

    me when i forgot about unix

--- src/rpc.h
+++ src/rpc.h
@@ -1,35 +1,22 @@
 #ifndef RPC_H
 #define RPC_H
+#include <sys/socket.h>
 
-enum connection {
-	SOCKET,
-	NETWORK,
-};
-
-struct local {
-	char *path;
-
+struct socket {
+	int type;
 	int fd;
-
-
-};
-
-struct remote {
-	int addr;
-	int port;
-
-	int fd;
-
-
-};
-
-struct remote_sys {
-	enum connection type;
 	union {
-		struct local loc;
-		struct remote rem;
+		struct sockaddr_un unix;
+		struct sockaddr_in ip;
 	};
 };
+
+socklen_t socklen(const struct socket* sock) {
+	if (sock->type == AF_INET) {
+		return sizeof(struct sockaddr_in);
+	}
+	return sizeof(struct sockaddr_un);
+}
 
 #endif /* RPC_H */
 
--- src/server.c
+++ src/server.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include <sys/socket.h>
+#include <stdboo.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc < 2) {
+		printf("defaulting to IP\n");
+	}
+
 	printf("server started\n");
+	int socket_desc = socket(AF_UNIX, SOCK_STREAM, 0);
 
 	return 0;
 }
